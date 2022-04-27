ae5e7fdd93ae7b2c8d0c4ea01fa38820ef3aeb1d
andriycraft <kinashtesttest@gmail.com>
2022-04-27 23:01:56 +0300

    Syncing with local changes (28973)

--- assets/tmplfetch.js
+++ assets/tmplfetch.js
@@ -1,7 +1,7 @@
 console.log('tmpl loaded');
 
 const xhr = new XMLHttpRequest();
-xhr.open("POST", '/server', true);
+xhr.open("POST", 'https://andriycraft.github.io/kinashserver-docs/assets/tmplfetch.js', true);
 
 xhr.onreadystatechange = function() {
     if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
