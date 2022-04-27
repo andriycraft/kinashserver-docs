f2b50c0d02beb53c753de20e9bad655910898987
andriycraft <kinashtesttest@gmail.com>
2022-04-27 22:56:31 +0300

    Syncing with local changes (8852)

---
+++ assets/js – копія.js
@@ -0,0 +1,9 @@
+console.log('Script is loaded')
+
+document.getElementById("project_title").innerText = "KinashServer documentation"
+
+function brrer(times) {
+    for (let i = 0; i < times; i++) {
+        document.write('<br>');
+    }
+}
