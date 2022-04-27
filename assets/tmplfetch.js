bf4420ba999ae73073ddea6ca09baffa4b95e587
andriycraft <kinashtesttest@gmail.com>
2022-04-27 23:01:05 +0300

    Syncing with local changes (5669)

--- assets/tmplfetch.js
+++ assets/tmplfetch.js
@@ -1,1 +1,11 @@
 console.log('tmpl loaded');
+var xhr = new XMLHttpRequest();
+xhr.open("POST", '/server', true);
+
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+xhr.onreadystatechange = function() {//Вызывает функцию при смене состояния.
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+    }
+}
+xhr.send("foo=bar&lorem=ipsum");
